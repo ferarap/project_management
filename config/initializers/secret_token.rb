c6fee6044d50a05e1b5d698e4ecaf36f17db4781
Pablo <ferarap@gmail.com>
2014-12-11 20:16:00 +0000

    create rails project with Ember support

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProjectManagement::Application.config.secret_key_base = 'fdd45b8f3dcdb0aef263ef000edd9ba0af50717b3c1d33d097068bb8309c0da83805a536c4dc694693e645a899c1a58b3e0abd6f1017dc47439ac78bb5509005'
